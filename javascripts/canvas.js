e2ccad892fafe5702031dd9f5c23ff82ee8d99d3
songzhj <songzhj@aliyun.com>
2015-08-14 22:11:05 +0800

    修复竖线
    修复了竖线(因为Firefox系列js获取到的height为1000px,要去掉px才能做参数).

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -2,6 +2,7 @@ function draw () {
 	var cvs = document.getElementById('cvs');
 	var ctx = cvs.getContext('2d');
 	var height = cvs.getAttribute('height');
+	height = height.substring(0, height.length - 2);
 	ctx.moveTo(710, 30);
 	ctx.lineTo(710, height);
 	ctx.moveTo(0, 457);
