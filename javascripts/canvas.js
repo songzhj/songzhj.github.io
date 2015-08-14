cbc15b83531188576e845276d96ea784f11f2d0a
songzhj <songzhj@aliyun.com>
2015-08-14 18:28:52 +0800

    优化一些细节
    优化了横线的像素点,y轴增加1px

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -2,11 +2,10 @@ function draw () {
 	var cvs = document.getElementById('cvs');
 	var ctx = cvs.getContext('2d');
 	var height = cvs.getAttribute('height');
-	ctx.beginPath();
 	ctx.moveTo(710, 30);
 	ctx.lineTo(710, height);
-	ctx.moveTo(0, 455);
-	ctx.lineTo(1000, 455);
+	ctx.moveTo(0, 456);
+	ctx.lineTo(1000, 456);
 	ctx.strokeStyle = 'rgb(0, 0, 0)';
 	ctx.stroke();
 }
