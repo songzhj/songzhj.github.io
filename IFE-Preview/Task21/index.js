98e95fb820912b05bf3d51782dc639837fb0ed66
宋治佳 <jia@songzhj.com>
2016-05-29 15:28:02 +0800

    update

--- IFE-Preview/Task21/index.js
+++ IFE-Preview/Task21/index.js
@@ -1,40 +1,89 @@
-var arrData = [];
+var tagData = [];
+var favoData = [];
+
 /*
- * 插入
+ * 插入tag
  */
-function insert() {
-	var textarea = document.getElementById('textarea');
+function insertTag(e) {
+	if (e.keyCode != 188 && e.keyCode != 32 && e.keyCode != 13) { //逗号，空格，回车
+		return;
+	}
+	var textarea = document.getElementById('tag-input');
 	var text = textarea.value;
-	arrData = text.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).filter(function(item) {
+	var arrData = text.split(/[,\s]+/).filter(function(item) {
 		return item.length > 0;
 	});
-	render(arrData);
+	for (var i = 0; i < arrData.length; ++i) {
+		if (tagData.indexOf(arrData[i]) < 0) {
+			tagData.push(arrData[i]);
+			if (tagData.length > 10) {
+				tagData.shift();
+			}
+		}
+	}
+	if (e.keyCode == 13) {
+		textarea.value = '';
+	}
+	renderTag(tagData);
 }
 
 /*
- * 绘制
+ * 插入favo
  */
-function render(arrData) {
-	var wrap = document.getElementById('wrap');
+function insertFavo() {
+	var textarea = document.getElementById('favo-input');
+	var text = textarea.value;
+	var arrData = text.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).filter(function(item) {
+		return item.length > 0;
+	});
+	for (var i = 0; i < arrData.length; ++i) {
+		if (favoData.indexOf(arrData[i]) < 0) {
+			favoData.push(arrData[i]);
+			if (favoData.length > 10) {
+				favoData.shift();
+			}
+		}
+	}
+
+	renderFavo(favoData);
+}
+
+/*
+ * 绘制tag
+ */
+function renderTag(arrData) {
+	var wrap = document.getElementById('tag-wrap');
 	var html = arrData.map(function(item) {
-		return "<div class='item'>" + item + "</div>";
+		return "<div class='tag-item'>" + item + "</div>";
 	}).join(" ");
 	wrap.innerHTML = html;
 }
 
 /*
- * 查找匹配
+ * 绘制favo
  */
- function find() {
- 	var findText = document.getElementById('find-text');
- 	var str = findText.value;
- 	var arrMatches = arrData.map(function(item) {
- 		return item.replace(str,"<span class='match'>" + str + "</span>");
- 	});
- 	render(arrMatches);
- }
+function renderFavo(arrData) {
+	var wrap = document.getElementById('favo-wrap');
+	var html = arrData.map(function(item) {
+		return "<div class='favo-item'>" + item + "</div>";
+	}).join(" ");
+	wrap.innerHTML = html;
+}
+
+/*
+ * 删除Tag
+ */
+function deleteTag(e) {
+	var node = e.target;
+	if (node.className == 'tag-item') {
+		var index = tagData.indexOf(node.innerHTML);
+		tagData.splice(index, 1);
+	}
+	renderTag(tagData);
+}
 
 (function() {
-	document.getElementById('insert').addEventListener('click', insert);
-	document.getElementById('find').addEventListener('click', find);
+	document.getElementById('add-favo').addEventListener('click', insertFavo);
+	document.getElementById('tag-input').addEventListener('keyup', insertTag);
+	document.getElementById('tag-wrap').addEventListener('click', deleteTag);
 })();
